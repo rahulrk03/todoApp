0b0e7df69c10109bfcee00bc18618055760ad6d7
Rahul Kumar <rahul.k@panorbit.in>
2021-01-09 01:08:24 +0530

    Delete functionality

--- src/App.js
+++ src/App.js
@@ -12,8 +12,8 @@ function App() {
 
   useEffect(() => {
     //this code here...fores when the app.js loads
-    db.collection('todos').onSnapshot(snapshot => {
-      setTodos(snapshot.docs.map(doc => doc.data().todo))
+    db.collection('todos').orderBy('timestamp' , 'desc').onSnapshot(snapshot => {
+      setTodos(snapshot.docs.map(doc =>({id:doc.id, todo: doc.data().todo})))
     })
   }, []);
 
@@ -46,7 +46,7 @@ function App() {
 
       <ul>
         {todos.map(todo => (
-          <Todo text={todo}/> 
+          <Todo todo={todo}/> 
           // <li>{todo}</li>
         
         ))}
--- src/Todo.js
+++ src/Todo.js
@@ -2,16 +2,19 @@
 import React from 'react';
 import './Todo.css';
 import { List, ListItem, ListItemText, ListItemAvatar } from '@material-ui/core';
+import db from './firebase';
+import DeleteForeverIcon from '@material-ui/icons/DeleteForever';
 
 function Todo(props) {
+
     return (
         <List className="todo_list">
             <ListItem>
                 <ListItemAvatar>
                 </ListItemAvatar>
-                <ListItemText primary={props.text} secondary="Dummy Deadline" />
+                <ListItemText primary={props.todo.todo} secondary="Dummy Deadline" />
             </ListItem>
-
+            <DeleteForeverIcon onClick={event => db.collection('todos').doc(props.todo.id).delete()} />
              {/* <li>{props.text}</li> */}
              </List>
     )
